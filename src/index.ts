db6415523f1b730250bc03df5a9dbeb96b475a9f
ldco2016 <renaissance.scholar2012@gmail.com>
2021-09-06 15:08:42 -0500

    Applied static methods to condense index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
-import { ConsoleReports } from "./reportTargets/ConsoleReport";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
-// Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader("football.csv");
-
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
+const matchReader = MatchReader.fromCsv("football.csv");
 const summary = Summary.winsAnalysisWithHtmlReport("Man United");
+matchReader.load();
 // matchReader.matches - this is the array of MatchData tuples
 summary.buildAndPrintReport(matchReader.matches);
